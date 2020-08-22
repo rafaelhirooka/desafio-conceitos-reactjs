651c8a252ad0e5ba80e935b461ffe3a32fe175c1
Rafael Hirooka <rafaelhirooka16@gmail.com>
2020-08-22 00:32:28 -0300

    add repository create and delete logic

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
+import Header from './components/Header';
+import api from './services/api';
+
+import './App.css';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [name, setName] = useState('');
+  const [link, setLink] = useState('');
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      "title": name,
+      "url": link,
+      "techs": techs
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  function handleRemoveRepository(repositoryId) {
+    api.delete(`repositories/${repositoryId}`);
+
+    setRepositories(repositories.filter(repository => repository.id != repositoryId));
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <>
+      <Header title="Repositórios" />
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul>
+        {repositories.map(repostitory => (
+          <li key={repostitory.id}>
+            {repostitory.title}
+            <button type="button" onClick={() => handleRemoveRepository(repostitory.id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+      <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Repository name" />
+      <br/>
+      <input type="text" value={link} onChange={e => setLink(e.target.value)} placeholder="Repository link" />
+      <br/>
+      <input type="text" value={techs} onChange={e => setTechs(e.target.value.split(','))} placeholder="Repository techs (separate it with commas)" />
+      <br/>
+      <button type="button" onClick={handleAddRepository}>Adicionar projeto</button>
+    </>
+  )
 }
 
 export default App;
